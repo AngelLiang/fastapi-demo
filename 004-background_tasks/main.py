64cabf725f5a31a52673acbb59380bd71f7c20d4
yannanxiu <ynanxiu@qq.com>
2023-07-18 10:46:11 +0800

    feat: 添加background_tasks

---
+++ 004-background_tasks/main.py
@@ -0,0 +1,20 @@
+from fastapi import BackgroundTasks, FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
+
+
+@app.post("/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host="127.0.0.1")
